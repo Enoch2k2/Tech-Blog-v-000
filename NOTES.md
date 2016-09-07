45483f4f966366f9ba3011610a22722f98c3be93
Enoch2k2 <enoch2k2@hotmail.com>
2016-09-07 10:53:18 -0500

    Added Devise, Angular-Devise, AuthController

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+#1. Install Devise.
+#2. Generate the Devise User.
+3. Setup Rails API backend
+4. Create the Sign up Log in pages.
+5. Setup Sign up / Log in Auth.
--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,10 +1,21 @@
 angular
-  .module('app', ['ui.router', 'templates'])
+  .module('app', ['ui.router', 'templates', 'Devise'])
   .config(function($stateProvider, $urlRouterProvider){
     $stateProvider
       .state('welcome', {
         url: '/',
-        templateUrl: 'app/templates/welcome.html'
+        templateUrl: 'app/templates/welcome.html',
+        controller: 'AuthController'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'app/templates/sign_up.html',
+        controller: 'AuthController'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/templates/login.html',
+        controller: 'AuthController'
       })
 
     $urlRouterProvider.otherwise('/');
